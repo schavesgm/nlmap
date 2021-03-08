96084469f8df497fa376d33c76361f9883d03eb0
Sergio Chaves Garcia-Mascaraque <sergiozteskate@gmail.com>
2021-03-08 16:41:27 +0000

    Main now takes command line arguments

--- main.cpp
+++ main.cpp
@@ -10,13 +10,20 @@
 
 int main(const int argc, const char** argv)
 {
-    // Added sigma noise to the map
-    const float sigma = 0.1;
+    // Load a Map file from memory
+    Map map(argv[1]);
 
-    // Some constants of the denoising algorithms
-    const float h2 = 0.05;
-    const float r_search = 2;
-    const float r_comp   = 1;
+    // Added sigma noise to the map
+    const float sigma = std::stof(argv[2]);
+
+    // Denoising h squared parameter
+    const float h2 = std::stof(argv[3]);
+
+    // Radious of comparison search in the denoiser
+    const float r_search = std::stof(argv[4]);
+
+    // Radious of comparison in the denoiser
+    const float r_comp = std::stof(argv[5]);
 
     // Generate the folder to save the data to
     auto out_path = Format::format_string(
@@ -27,16 +34,13 @@ int main(const int argc, const char** argv)
     // Create the output path if needed
     mkdir(out_path.c_str(), 0777);
 
-    // Load a Map file from memory
-    Map map("./data/rnase/refmac.map");
-
     // Add some noise to the map
     map.add_noise(sigma);
 
     // Denoise the map using non-local means
     Map denoised = map.nlmeans_denoise(h2, r_search, r_comp);
 
-    // Save the noise and denoised in memory
+    // Save the noisy and denoised maps in memory
     map.save_map(out_path + "/noisy.map");
     denoised.save_map(out_path + "/denoised.map");
 
