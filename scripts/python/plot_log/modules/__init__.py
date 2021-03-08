3b4e048f05e17408e394d9f72f5498989be46bb9
Sergio Chaves Garcia-Mascaraque <sergiozteskate@gmail.com>
2021-03-08 16:07:26 +0000

    Added script to plot data from log

---
+++ scripts/python/plot_log/modules/__init__.py
@@ -0,0 +1,3 @@
+# Import the plotting functions
+from .plots import plot_parameters
+from .plots import plot_residues
---
+++ scripts/python/plot_log/plot_log.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import os, sys
+
+# Load the JSON parser module
+from modules import plot_parameters
+from modules import plot_residues
+
+if __name__ == '__main__':
+
+    # Select the log path to use
+    log_path = sys.argv[1]
+
+    # Plot the parameters form the log file
+    plot_parameters(log_path)
+
+    # Plot the residues form the log file
+    plot_residues(log_path)
