66237d8928f33a0b3ec820d76fc71b4c66502012
Sergio Chaves Garcia-Mascaraque <sergiozteskate@gmail.com>
2021-05-06 14:59:09 +0000

    Minor changes and added help message

---
+++ scripts/shell/help.sh
@@ -0,0 +1,22 @@
+function print_help()
+{
+    printf " -- pipeline script\n"
+    printf "    ./pipeline [protein] [sigma] [perc_h] [r_env] [pref_eps]\n\n"
+    printf "  * Parameters:\n"
+    printf "     * protein: Name of the protein. The program needs all proteins\n"
+    printf "       to be processed located inside a data/ folder. The data folder\n"
+    printf "       must be present in the same directory where pipeline is called.\n"
+    printf "     * sigma: Standard deviation of the additive gaussian noise added\n"
+    printf "       to the map. sigma = 0.0 will not add any noise.\n"
+    printf "     * perc_h: Percentage of the total spread of the map that will be\n"
+    printf "       used to generate the denoising parameter.\n"
+    printf "     * r_env: Radious that defines the environment volume. All points\n"
+    printf "       whose distance is less or equal than r_env from a reference point\n"
+    printf "       will be considered in the environment calculation.\n"
+    printf "     * pref_eps: Prefilter size.\n\n"
+    printf "  * Important:\n"
+    printf "     - pipeline needs a environmental variable called \$CCP4_PATH\$\n"
+    printf "    containing the path to the CCP4 software. For example, it can\n"
+    printf "    be CCP4_PATH=~/software/ccp4-7.1.\n"
+    printf " --\n"
+}
